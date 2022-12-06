5bcdcf3b93d7a059be5f5d24df9d1af9eb0679ba
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-12-06 14:28:25 +0100

    Add files via upload

---
+++ Option 3 school.py
@@ -0,0 +1,11 @@
+Usernumber = int(input("enter a number between 0-3"))
+if (Usernumber == 0):
+    print("Coding is fun")
+elif (Usernumber == 1):
+    print("Python is not code, but a way of life")
+elif (Usernumber == 2):
+    print("We learn Python the fun way")
+elif (Usernumber == 3):
+    print("We code, because we can")
+else:
+    print("No valid selection")
