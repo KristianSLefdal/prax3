afc9eafc6939077fa4096d197f797f658cdc855e
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-12-06 14:14:51 +0100

    Add files via upload

---
+++ number guesser.py
@@ -0,0 +1,8 @@
+usernumber = int(input("please input a number"))
+if (usernumber == 543):
+    print("CORRECT")
+    print("YOU ARE THE WINNER")
+elif (usernumber < 543):
+    print("TOO LOW NUMBER")
+else:
+    print("TOO HIGH")
