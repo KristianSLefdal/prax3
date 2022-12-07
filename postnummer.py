836cb8ed33256534fe744548e88e636978a20b74
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-12-07 14:23:36 +0100

    Add files via upload

---
+++ postnummer.py
@@ -0,0 +1,6 @@
+postcodes ={7013 : "Trondheim", 6415 : "Molde",637 : "Oslo",8450 : "Stokmarknes", 7340 : "Oppdal",
+6783 : "Stryn", 4005 : "Stavanger", 5073 : "Bergen",  }
+
+pcentered = int(input("enter a valid postcode: "))
+if pcentered in postcodes.keys():
+    print("postcode:",pcentered, "-",postcodes[pcentered], "\n")
